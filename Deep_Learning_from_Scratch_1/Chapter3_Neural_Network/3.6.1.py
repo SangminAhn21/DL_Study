36af2f3c68aee660c96ae5f28661dea157fd2022
SangminAhn <alsbmj012123@gmail.com>
2021-09-05 14:20:51 +0000

    정진

---
+++ Deep_Learning_from_Scratch_1/Chapter3_Neural_Network/3.6.1.py
@@ -0,0 +1,12 @@
+import sys
+import os
+from mnist import load_mnist
+
+sys.path.append(os.pardir)
+(x_train, t_train), (x_test, t_test) = \
+    load_mnist(flatten=True, normalize=False)
+
+print(x_train.shape)
+print(t_train.shape)
+print(x_test.shape)
+print(t_test.shape)
