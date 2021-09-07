66f31c6d377366dacb70015f52de97f9d3157d7b
SangminAhn <alsbmj012123@gmail.com>
2021-09-07 08:59:38 +0000

    정진

--- Deep_Learning_from_Scratch_1/Chapter4_Neural_Network_Training/4.2.3.py
+++ Deep_Learning_from_Scratch_1/Chapter4_Neural_Network_Training/4.2.3.py
@@ -8,7 +8,7 @@ sys.path.append(os.pardir)
 
 (x_train, t_train), (x_test, t_test) =\
     load_mnist(normalize=True, one_hot_label=True)
-    
+
 print(x_train.shape)
 print(t_train.shape)
 
@@ -17,3 +17,5 @@ batch_size = 10
 batch_mask = np.random.choice(train_size, batch_size)
 x_batch = x_train[batch_mask]
 t_batch = t_train[batch_mask]
+
+print(batch_mask)
