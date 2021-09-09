c5dd3506690b4fa6c3cf0cb52209c5d2a02cd99d
SangminAhn <alsbmj012123@gmail.com>
2021-09-09 14:24:16 +0000

    정진

--- Deep_Learning_from_Scratch_1/Chapter4_Neural_Network_Training/two_layer_net.py
+++ Deep_Learning_from_Scratch_1/Chapter4_Neural_Network_Training/two_layer_net.py
@@ -51,7 +51,7 @@ class TwoLayerNet:
 
         return grads
 
-
+'''
 net = TwoLayerNet(input_size=784, hidden_size=100, output_size=10)
 print(net.params['W1'].shape)
 print(net.params['b1'].shape)
@@ -61,7 +61,7 @@ print(net.params['b2'].shape)
 x = np.random.rand(100, 784)
 y = net.predict(x)
 print(y)
-
+'''
 
 
 
