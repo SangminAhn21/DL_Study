b12533d6fce6690d0fe1e8ef9bbff6bb5bd3b508
SangminAhn <alsbmj012123@gmail.com>
2021-10-18 11:49:06 +0000

    정진

---
+++ Deep_Learning_from_Scratch_2/Chapter7_Sentence_Creation_with_RNN/generate_text.py
@@ -0,0 +1,21 @@
+import sys
+sys.path.append('/workspace/DL_Study/Deep_Learning_from_Scratch_2/DLfromScratch_2')
+from rnnlm_gen import RnnlmGen
+from dataset import ptb
+
+corpus, word_to_id, id_to_word = ptb.load_data('train')
+vocab_size = len(word_to_id)
+corpus_size = len(corpus)
+
+model = RnnlmGen()
+model.load_params('/workspace/DL_Study/Deep_Learning_from_Scratch_2/DLfromScratch_2/ch06/Rnnlm.pkl')
+
+start_word = 'you'
+start_id = word_to_id[start_word]
+skip_words = ['N', '<unk>', '$']
+skip_ids = [word_to_id[w] for w in skip_words]
+
+word_ids = model.generate(start_id, skip_ids)
+txt = ' '.join([id_to_word[i] for i in word_ids])
+txt = txt.replace(' <eos>', '.\n')
+print(txt)
