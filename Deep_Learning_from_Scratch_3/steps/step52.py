9a0626fb27330c1b908f199362e9a974651ecded
SangminAhn <alsbmj012123@gmail.com>
2021-10-28 09:39:07 +0000

    정진

--- Deep_Learning_from_Scratch_3/steps/step52.py
+++ Deep_Learning_from_Scratch_3/steps/step52.py
@@ -8,18 +8,21 @@ from dezero import optimizers, DataLoader
 import dezero.functions as F
 from dezero.models import MLP
 
-max_epoch = 300
-batch_size = 30
-hidden_size = 10
-lr = 1.0
+max_epoch = 5
+batch_size = 100
+hidden_size = 1000
 
-train_set = dezero.datasets.Spiral(train=True)
-test_set = dezero.datasets.Spiral(train=False)
+train_set = dezero.datasets.MNIST(train=True)
+test_set = dezero.datasets.MNIST(train=False)
 train_loader = DataLoader(train_set, batch_size)
 test_loader = DataLoader(test_set, batch_size, shuffle=False)
 
-model = MLP((hidden_size, 3))
-optimizer = optimizers.SGD(lr).setup(model)
+model = MLP((hidden_size, 10))
+optimizer = optimizers.SGD().setup(model)
+
+if dezero.cuda.gpu_enable:
+    train_loader.to_gpu()
+    model.to_gpu()
 
 for epoch in range(max_epoch):
     sum_loss, sum_acc = 0, 0
@@ -35,9 +38,9 @@ for epoch in range(max_epoch):
         sum_loss += float(loss.data) * len(t)
         sum_acc += float(acc.data) * len(t)
 
-    print('epoch: {}'.format(epoch+1))
+    print('epoch: {}'.format(epoch + 1))
     print('train loss: {:.4f}, accuracy: {:.4f}'.format(
-        sum_loss / len(test_set), sum_acc / len(test_set)))
+        sum_loss / len(train_set), sum_acc / len(train_set)))
 
     sum_loss, sum_acc = 0, 0
     with dezero.no_grad():
@@ -45,6 +48,7 @@ for epoch in range(max_epoch):
             y = model(x)
             loss = F.softmax_cross_entropy(y, t)
             acc = F.accuracy(y, t)
+
             sum_loss += float(loss.data) * len(t)
             sum_acc += float(acc.data) * len(t)
 
