62f56579deb3125a7599afaa7c4e8eb7b4d9a5d7
SangminAhn <alsbmj012123@gmail.com>
2021-10-29 10:40:22 +0000

    정진

---
+++ Deep_Learning_from_Scratch_3/steps/step58.py
@@ -0,0 +1,24 @@
+import numpy as np
+if '__file__' in globals():
+    import os, sys
+    sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+from PIL import Image
+import dezero
+from dezero.models import VGG16
+
+url = 'https://github.com/oreilly-japan/deep-learning-from-scratch-3/raw/images/zebra.jpg'
+img_path = dezero.utils.get_file(url)
+img = Image.open(img_path)
+x = VGG16.preprocess(img)
+print(x.shape)
+x = x[np.newaxis]
+print(x.shape)
+
+model = VGG16(pretrained=True)
+with dezero.test_mode():
+    y = model(x)
+predict_id = np.argmax(y.data)
+
+model.plot(x, to_file='vgg.pdf')
+labels = dezero.datasets.ImageNet.labels()
+print(labels[predict_id])
